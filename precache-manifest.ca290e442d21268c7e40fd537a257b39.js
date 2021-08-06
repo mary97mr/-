a5fd8daf2734b85b18126ed5de1bac25cfab00b7
Mari <marymendirojas@gmail.com>
2021-08-06 07:54:52 +0200

    Updates

---
+++ precache-manifest.ca290e442d21268c7e40fd537a257b39.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1cab128e1bc2cff5fbd713d0c29c4289",
+    "url": "/mary97mr.github.io/dev-portfolio/index.html"
+  },
+  {
+    "revision": "66e3123e5b4faaca5421",
+    "url": "/mary97mr.github.io/dev-portfolio/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "d3e6a4100fc9639f59ee",
+    "url": "/mary97mr.github.io/dev-portfolio/static/css/main.39cff0c2.chunk.css"
+  },
+  {
+    "revision": "66e3123e5b4faaca5421",
+    "url": "/mary97mr.github.io/dev-portfolio/static/js/2.46bc056b.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/mary97mr.github.io/dev-portfolio/static/js/2.46bc056b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d3e6a4100fc9639f59ee",
+    "url": "/mary97mr.github.io/dev-portfolio/static/js/main.160bc511.chunk.js"
+  },
+  {
+    "revision": "d5eef9cc76bcfec1cb4a",
+    "url": "/mary97mr.github.io/dev-portfolio/static/js/runtime-main.4b811bbb.js"
+  }
+]);
